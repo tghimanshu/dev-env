a7b058329b7268883b5a640b4fda61b97f4922d6
Himanshu Gohil <himanshugohil234@gmail.com>
2026-04-17 23:37:23 +0530

    installing flutter

--- omarchy/install-flutter.sh
+++ omarchy/install-flutter.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 yay -S --needed --noconfirm flutter
+
+# Linux Drivers
+yay -S --needed --noconfirm mesa-utils
+
+yay -S --needed --noconfirm android-studio
+
+sudo groupadd flutterusers
+sudo gpasswd -a $USER flutterusers
+sudo chown -R :flutterusers /usr/lib/flutter
+sudo chmod -R g+w /usr/lib/flutter
+
+yes | sudo /opt/android-sdk/cmdline-tools/latest/bin/sdkmanager --licenses
