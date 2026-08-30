5d31aefbe01639b93ce8eaea6b1f8090cd983f27
Himanshu Gohil <himanshugohil234@gmail.com>
2026-08-30 23:39:29 +0530

    Added Mac Setups

---
+++ mac/install-tools.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+brew install --cask ghostty
+brew install stow
+brew install neovim
+brew install tmux
+brew install fzf
+brew install oven-sh/bun/bun
+brew install cmake
+brew install nvm
+brew install stylua
+brew install gh
+brew install lazygit
+brew install luarocks
+brew install lsd
+brew install imagemagick
+brew install rust
+brew install yazi
+brew install zoxide ripgrep fd poppler jq unar ffmpegthumbnailer
+brew install go
+brew install --cask visual-studio-code
